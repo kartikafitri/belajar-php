7a061c799c30292c9927e30f15953b39cf3a714a
kartikafitri <kartikaf67@gmail.com>
2022-10-24 11:41:36 +0700

    note/memberi pesan

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 echo"Hello World!";
 
 $nama = "Kartika";
-$umur = 20;
-echo "Nama saya <strong>$nama</strong, saya berusia $umur tahun.<br>"; 
+$umur = 21 ;
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>"; 
 
 $namaAdik = "Diaz"; 
+$umurAdik = 19 ;
 
+$selisihUmur = $umur-$umurAdik ;
+echo "nama adik saya <strong> $namaAdik</strong>, adik saya berusia $umurAdik tahun.<br> Selisih umur saya dan adik saya $selisihUmur";
 
-?>
+?> 
